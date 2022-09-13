882b09948a78328abe5c404237a86d81b25e7104
anyamittal08 <anyamittal08@gmail.com>
2022-09-13 16:55:44 -0400

    added customizability and styling

--- index.js
+++ index.js
@@ -1,49 +1,63 @@
 const container = document.querySelector(".container");
+const button = document.querySelector('button');
+let squares;
 
-function makeRows(n) {
+const setBackgroundToWhite = () => {
+  squares.forEach((square) => {
+    square.style.backgroundColor = 'rgb(255, 255, 255)';  
+  })
+};
+
+const handleHover = () => {
+  squares.forEach((square) => {
+      square.addEventListener('mouseenter', () => {
+          let currentColor = square.style.backgroundColor;
+          let newColor = calculateNewRgb(currentColor);
+  
+          square.style.backgroundColor = newColor;
+      })
+  });
+};
+
+const calculateNewRgb = (color) => {
+
+  let indexOfFirstComma = color.indexOf(',');
+  let indexOfSecondComma = color.indexOf(',', color.indexOf(',') + 1);
+  let indexOfCloseParens = color.indexOf(')');
+
+  let r = parseFloat(color.slice(4, indexOfFirstComma));
+  let g = parseFloat(color.slice(indexOfFirstComma + 1, indexOfSecondComma));
+  let b = parseFloat(color.slice(indexOfSecondComma + 1, indexOfCloseParens));
+
+  if (r <= 0 || g <= 0 || b <= 0) return;
+
+  let newR = r - 25.5;
+  let newG = g - 25.5;
+  let newB = b - 25.5;
+
+  let newRgb = 'rgb(' + newR + ',' + newG + ',' + newB + ')' ;
+
+  return newRgb;
+};
+
+const customizeGrid = () => {
+  let userVal = prompt('Please enter the desired number of rows (1-100)');
+  makeGrid(parseInt(userVal));
+};
+
+const makeGrid = (n) => {
   container.style.setProperty('--grid-rows', n);
   container.style.setProperty('--grid-cols', n);
   for (c = 0; c < (n * n); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
-  };
+    container.appendChild(cell).className = "square";
+  }
+  squares = document.querySelectorAll('.square');
+
+  setBackgroundToWhite();
+  handleHover();
 };
 
-makeRows(50);
+makeGrid(16);
 
-const calculateNewRgb = (color) => {
-
-    let indexOfFirstComma = color.indexOf(',');
-    let indexOfSecondComma = color.indexOf(',', color.indexOf(',') + 1);
-    let indexOfCloseParens = color.indexOf(')')
-
-    let r = parseFloat(color.slice(4, indexOfFirstComma));
-    let g = parseFloat(color.slice(indexOfFirstComma + 1, indexOfSecondComma));
-    let b = parseFloat(color.slice(indexOfSecondComma + 1, indexOfCloseParens));
-
-    if (r <= 0 || g <= 0 || b <= 0) return;
-
-    let newR = r - 25.5;
-    let newG = g - 25.5;
-    let newB = b - 25.5;
-
-    let newRgb = 'rgb(' + newR + ',' + newG + ',' + newB + ')' 
-
-    return newRgb;
-}
-
-const squares = document.querySelectorAll('.grid-item');
-
-squares.forEach((square) => {
-    square.style.backgroundColor = 'rgb(255, 255, 255)';  
-})
-
-squares.forEach((square) => {
-    square.addEventListener('mouseenter', () => {
-        let currentColor = square.style.backgroundColor;
-        let newColor = calculateNewRgb(currentColor);
-
-        square.style.backgroundColor = newColor;
-    })
-})
-
+button.addEventListener('click', customizeGrid);
